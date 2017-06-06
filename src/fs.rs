d6a4768cb0f626349f72902dc8afaa233e2d9e82
Boscop <kingboscop@gmail.com>
2017-06-06 12:55:03 +0200

    check if GetModuleFileNameW() succeeded, and pass correct path len

--- src/fs.rs
+++ src/fs.rs
@@ -3,7 +3,7 @@ use std::path::{Path, PathBuf};
 #[cfg(windows)]
 pub fn get_folder_path() -> Option<PathBuf> {
 	use winapi::*;
-	use kernel32;
+	use kernel32::*;
 
 	use std::ptr::null_mut;
 	use std::mem;
@@ -13,17 +13,22 @@ pub fn get_folder_path() -> Option<PathBuf> {
 	const GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS: DWORD = 0x00000004;
 	const GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT: DWORD = 0x00000002;
 
-	unsafe {
-		let mut hm: HMODULE = null_mut();
+	let mut hm: HMODULE = null_mut();
 
-		if kernel32::GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, &get_folder_path as *const _ as LPCWSTR, &mut hm as *mut _) == 0 {
-			error!("GetModuleHandle returned {}", kernel32::GetLastError());
+	unsafe {
+		if GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, &get_folder_path as *const _ as LPCWSTR, &mut hm as *mut _) == 0 {
+			error!("GetModuleHandleExW() failed with {}", GetLastError());
 			None
 		} else {
 			let mut path: [WCHAR; MAX_PATH + 1] = mem::zeroed();
-			let len = kernel32::GetModuleFileNameW(hm, path.as_mut_ptr(), mem::size_of_val(&path) as u32) as usize;
-			let file_path = OsString::from_wide(&path[..len]);
-			Path::new(&file_path).parent().map(Into::into)
+			let len = GetModuleFileNameW(hm, path.as_mut_ptr(), path.len() as u32) as usize;
+			if len == 0 {
+				error!("GetModuleFileNameW() failed with {}", GetLastError());
+				None
+			} else {
+				let file_path = OsString::from_wide(&path[..len]);
+				Some(Path::new(&file_path).parent().unwrap().into())
+			}
 		}
 	}
 }
